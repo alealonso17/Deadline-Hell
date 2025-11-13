cbf016a1e2e35d339bf9a44e0bdeaa040d3a51cd
alealonso17 <2alejandroalonso70@gmail.com>
2025-11-13 23:33:27 +0000

    add backend and frontend

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,23 +1,19 @@
 import mysql from "mysql2/promise";
-try {
-  const connection = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    ssl: {
-      rejectUnauthorized: true
-    }
 
-  });
-  
-  console.log("Conection with mysql ok ✔️"); 
-  
+const connection = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
 
-}catch{
-  console.log("Conection with mysql NULL");  
-}
+});
+
+console.log("Conection with mysql ok ✔️");
+
 
 export default connection;
 
